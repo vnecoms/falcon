5456ba6bb26b574811b2581021d8ef5f4dd087f5
Oleksandr Bordun <bordun.alexandr@gmail.com>
2018-10-09 11:14:19 +0200

    fix(falcon-magento2-api): providing tests for unauthenticated requests

--- packages/falcon-magento2-api/src/index.test.js
+++ packages/falcon-magento2-api/src/index.test.js
@@ -3,6 +3,7 @@ global.__SERVER__ = true; // eslint-disable-line no-underscore-dangle
 const nock = require('nock');
 const Magento2Api = require('./index');
 const magentoResponses = require('./__mocks__/apiResponses');
+const { codes } = require('@deity/falcon-errors');
 
 const URL = 'http://example.com';
 const apiConfig = {
@@ -12,10 +13,7 @@ const apiConfig = {
   },
   name: 'api-magento2'
 };
-
-function createMagentoUrl(path) {
-  return `/rest/default/V1${path}`;
-}
+const createMagentoUrl = path => `/rest/default/V1${path}`;
 
 describe('Magento2Api', () => {
   let api;
@@ -32,12 +30,12 @@ describe('Magento2Api', () => {
     nock.restore();
   });
 
-  it('should correctly fetch admin token', async () => {
+  it('Should correctly fetch admin token', async () => {
     const result = await api.retrieveAdminToken();
     expect(result).toEqual(magentoResponses.adminToken.data.token);
   });
 
-  it('should correctly fetch category data', async () => {
+  it('Should correctly fetch category data', async () => {
     nock(URL)
       .get(uri => /\/categories\/20/.test(uri)) // regexp as query params might be there
       .reply(200, magentoResponses.category.data);
@@ -47,5 +45,22 @@ describe('Magento2Api', () => {
     expect(result.data.id).toEqual(magentoResponses.category.data.id);
   });
 
-  // todo: detailed tests for category data parsing, so all the cases are covered
+  it('Should correctly handle request error without token', async () => {
+    nock(URL)
+      .get(uri => uri.indexOf(createMagentoUrl('/products') > 0))
+      .reply(401, {
+        message: 'Consumer is not authorized to access %resources',
+        parameters: {
+          resources: 'Magento_Catalog::categories'
+        }
+      });
+
+    try {
+      await api.products({});
+    } catch (error) {
+      expect(error.extensions.code).toBe(codes.UNAUTHENTICATED);
+      expect(error.message).toBe('Consumer is not authorized to access Magento_Catalog::categories');
+      expect(error.extensions.response.status).toBe(401);
+    }
+  });
 });
